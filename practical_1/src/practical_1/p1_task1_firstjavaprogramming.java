b98da12b66ce97c96031e2a9446a3b911de00afb
hitsagarwal <96682132+hitsagarwal@users.noreply.github.com>
2022-04-02 13:48:04 +0530

    first time commit

---
+++ practical_1/src/practical_1/p1_task1_firstjavaprogramming.java
@@ -0,0 +1,10 @@
+package practical_1;
+
+public class p1_task1_firstjavaprogramming {
+	public p1_task1_firstjavaprogramming () {
+    }
+
+    public static void main(String[] var0) {
+        System.out.println("hello world");
+    }
+}
